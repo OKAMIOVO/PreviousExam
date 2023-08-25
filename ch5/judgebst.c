382f6c835b58ab7982e22f833ed5c71d08062e2a
OKAMI <2717806614@qq.com>
2023-08-25 08:24:48 +0800

    fourth

--- ch5/judgebst.c
+++ ch5/judgebst.c
@@ -21,7 +21,7 @@ bool judgeInorderBST(SqList bt, int k, int* val) { // 初始调用时k的值是0
             return false;                         // 左子树不符合定义
         if (bt.data[k] <= *val)                   // 当前节点
             return false;  // 前驱大于当前节点，不符合定义
-        *val = bt.data[k]; // 更新val
+        *val = bt.data[k]; // 更新前驱val
         if (!judgeInorderBST(bt, 2 * k + 2, val)) // 递归调用右子树
             return false;                         // 右子树不符合定义
     }
